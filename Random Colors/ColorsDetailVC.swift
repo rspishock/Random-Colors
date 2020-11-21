f96cb95849eb2a6c5b020f4a61e9ff33dd0f7eeb
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-11-20 19:08:31 -0500

    Add TableView and ColorDetailVC

---
+++ Random Colors/ColorsDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  ColorsDetailVC.swift
+//  Random Colors
+//
+//  Created by Ryan Spishock on 11/20/20.
+//
+
+import UIKit
+
+class ColorsDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
